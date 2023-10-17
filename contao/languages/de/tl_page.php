ee51dd0237aa86f1315ac1df0f92a4a9390c120e
Thomas Körner <t.koerner@heimrich-hannot.de>
2023-10-17 14:26:15 +0200

    add translations

---
+++ contao/languages/de/tl_page.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang = &$GLOBALS['TL_LANG']['tl_page'];
+
+/**
+ * Fields
+ */
+$lang['ipProtected'][0] = 'Seite anhand von IP-Adressen schützen';
+$lang['ipProtected'][1] = 'Den Seiten-Zugriff auf bestimmte IP-Adressen bzw. -Subnetze beschränken. Wird die Seite von einem Endgerät mit einer erlaubten IP-Adresse aufgerufen, wird der Zugriff erlaubt, auch wenn der Nutzer nicht als Mitglied angemeldet ist oder sich in einer freigegebenen Mitgliedergruppe befindet.';
+$lang['allowedIps'][0] = 'Erlaubte IP-Adressen bzw. -Subnetze';
+$lang['allowedIps'][1] = 'Geben Sie hier die IP-Adressen bzw. -Subnetze an. Sie können sowohl IPv4- als auch IPv6-Adressen und -Subnetze verwenden.';
---
+++ contao/languages/en/tl_page.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang = &$GLOBALS['TL_LANG']['tl_page'];
+
+/**
+ * Fields
+ */
+$lang['ipProtected'][0] = 'Protect Page Based on IP Addresses';
+$lang['ipProtected'][1] = 'Restrict page access to specific IP addresses or subnets. If the page is accessed from a device with an approved IP address, access is granted, even if the user is not logged in as a member or is part of an authorized member group.';
+$lang['allowedIps'][0] = 'Allowed IP Addresses or Subnets';
+$lang['allowedIps'][1] = 'Specify the IP addresses or subnets here. You can use both IPv4 and IPv6 addresses and subnets.';
